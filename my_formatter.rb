7057b8ff8dde8956c8ce4d41dd3bb06643a6e083
kei-kamiguchi <56471454+kei-kamiguchi@users.noreply.github.com>
2021-09-20 14:51:57 +0900

    Create my_formatter.rb

---
+++ my_formatter.rb
@@ -0,0 +1,32 @@
+class MyFormatter
+  RSpec::Core::Formatters.register self, :example_passed, :example_failed, :example_group_started, :start
+  def initialize(output)
+    @output = output
+  end
+
+  def start(notification)
+    @output << "---\ntitle: 課題評価のフィードバック\n---\n"
+  end
+
+  def example_passed(notification)
+    @output << "✅ #{notification.example.description}\n"
+  end
+
+  def example_group_started(notification)
+    if notification.group.to_s.count(':') == 4
+      @output << "### #{notification.group.description}\n"
+    elsif notification.group.to_s.count(':') == 6
+      @output << "#### #{notification.group.description}\n"
+    elsif notification.group.to_s.count(':') == 8
+      @output << "- #{notification.group.description}\n"
+    elsif notification.group.to_s.count(':') == 10
+      @output << "  - #{notification.group.description}\n"
+    else
+      @output << "    - #{notification.group.description}\n"
+    end
+  end
+
+  def example_failed(notification)
+    @output << "- [ ] ❌ #{notification.example.description}\n"
+  end
+end
