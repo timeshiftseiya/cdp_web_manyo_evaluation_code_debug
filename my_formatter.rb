8f6dc97dc2d5215ae72c49e216b373417b17660f
kei-kamiguchi <56471454+kei-kamiguchi@users.noreply.github.com>
2021-09-26 19:06:11 +0900

    Update my_formatter.rb

--- my_formatter.rb
+++ my_formatter.rb
@@ -13,12 +13,12 @@ class MyFormatter
   end
 
   def example_group_started(notification)
-    if notification.group.to_s.count(':') == 6
-      @output << "### #{notification.group.description}\n"
+    if notification.group.to_s.count(':') == 4
+      @output << "# #{notification.group.description}\n"
     elsif notification.group.to_s.count(':') == 8
-      @output << "###### #{notification.group.description}\n"
+      @output << "### {notification.group.description}\n"
     elsif notification.group.to_s.count(':') == 10
-      @output << "- ######{notification.group.description}\n"
+      @output << "###### {notification.group.description}\n"
     end
   end
 
